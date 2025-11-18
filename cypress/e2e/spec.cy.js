7bcd259f24b1c4bb02ec85ff7ff13c571733cfc8
renatobss <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>
2025-11-17 22:03:35 -0300

    segundo commit

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -32,4 +32,27 @@ describe('Teste de Login', () => {
     cy.get('.alert').contains('Password é obrigatório')
   })
 
+  it.only('Cadastrando usuário', () => {
+    cy.visit('https://front.serverest.dev/login')
+    cy.get('[data-testid="email"]').type('renato@teste.com.br')
+    cy.get('[data-testid="senha"]').type('123')
+    cy.get('[data-testid="entrar"]').click()
+
+    //clicar para cadastrar o usuário
+    cy.get('[data-testid="cadastrarUsuarios"]').click()
+    cy.get('h1').contains('Cadastro de usuários')
+
+    //informar os dados do novo usuário
+    cy.get('[data-testid="nome"]').type('juca')
+    cy.get('[data-testid="email"]').type('juca@teste.com.br')
+    cy.get('[data-testid="password"]').type('123')
+    cy.get('[data-testid="cadastrarUsuario"]').click()
+    cy.wait(5000)
+    cy.get('h1').contains('Lista dos usuários')
+
+    //próximo dia verificar quando não consegue cadastrar o usuário, pois dá erro quando tento listar os usuários (Inserir verificação para garantir)
+    //que somente tente listar os usuário quando conseguir cadastrar.
+
+  })
+
 })
